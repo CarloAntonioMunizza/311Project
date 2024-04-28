c005c559202f4c5d590b6881d8dc2f5c432c01e3
jack <jbl6489@psu.edu>
2024-04-27 21:01:02 -0400

    minor additions and push test
    -game state class

---
+++ src/GameState.java
@@ -0,0 +1,14 @@
+public class GameState {
+
+    //this should have higher level variables and the file IO
+    int turnsElapsed;
+
+    static void updateState() {
+        //updates the game critical variables
+    }
+
+    static void newTurn() {
+        //call everything that needs to happen every time step
+    }
+
+}
--- src/PlanetFolder/Planet.java
+++ src/PlanetFolder/Planet.java
@@ -6,12 +6,62 @@ import java.util.Random;
 
 public abstract class Planet {
 	protected int upgrades[][] = new int[6][3];
-	protected int inventroy[] = new int[6];
+	protected int inventory[] = new int[6];
 	protected PlanetType type;
 	protected String name;
 	protected int size;
 	protected int population;
 	protected HashMap<ResourceType, Integer> resources = new HashMap<>();
+
+	public int[][] getUpgrades() {
+		return upgrades;
+	}
+
+	public void setUpgrades(int[][] upgrades) {
+		this.upgrades = upgrades;
+	}
+
+	public int[] getInventory() {
+		return inventory;
+	}
+
+	public void setInventory(int[] inventory) {
+		this.inventory = inventory;
+	}
+
+	public PlanetType getType() {
+		return type;
+	}
+
+	public void setType(PlanetType type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+
 
 	public Planet(PlanetType type, String name, int size, int population) {
 		this.type = type;
@@ -21,11 +71,17 @@ public abstract class Planet {
 		initilizeResources();
 	}
 
+
+
 	protected abstract void initilizeResources();
 
 	protected int generateRandomResourceAmount(int base, int variance) {
 		Random random = new Random();
 		return base + random.nextInt(variance);
+	}
+
+	void resourceExtraction() {
+		//logic to gain resources based on extraction rate and density
 	}
 
 	@Override
